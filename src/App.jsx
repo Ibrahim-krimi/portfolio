ef9ce5456827d32b592ee68d18c6c567327713ae
ibrahim <ibrahimkrimi2@gmail.com>
2024-04-13 15:24:37 +0200

    test deploy azur

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas,Study } from "./components";
-
+//test
 const App = () => {
   return (
     <BrowserRouter>
